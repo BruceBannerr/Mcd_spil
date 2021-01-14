1e921a1ef44ca59c3ef8ccdd33a4943d6dddda4b
ÆLIne <jhek2014@gmail.com>
2021-01-14 09:14:41 +0100

    create coins

--- BoardGame.py
+++ BoardGame.py
@@ -66,12 +66,32 @@ class MyGame(arcade.Window):
 
         # Sprite lists
         self.player_list = arcade.SpriteList()
+        self.coin_list = arcade.SpriteList()
+
+        # Score
+        self.score = 0
 
         # Set up the player
         self.player_sprite = Player(":resources:images/animated_characters/female_person/femalePerson_idle.png", SPRITE_SCALING)
         self.player_sprite.center_x = 50
         self.player_sprite.center_y = 50
         self.player_list.append(self.player_sprite)
+
+
+        # Create the coins
+    for i in range(COIN_COUNT):
+
+        # Create the coin instance
+        # Coin image from kenney.nl
+        coin = arcade.Sprite(":resources:images/coin_01.png", SPRITE_SCALING_COIN)
+
+        # Position the coin
+        coin.center_x = random.randrange(SCREEN_WIDTH)
+        coin.center_y = random.randrange(SCREEN_HEIGHT)
+
+        # Add the coin to the lists
+        self.coin_list.append(coin)
+
 
     def on_draw(self):
         """
