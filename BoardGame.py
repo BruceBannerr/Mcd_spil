0fd6b7bf23b10169ee9122420e0e77765ef2a003
christinahsn <chri16g7@study.sde.dk>
2021-01-14 09:37:19 +0100

    Aline character

--- BoardGame.py
+++ BoardGame.py
@@ -62,9 +62,9 @@ class MyGame(arcade.Window):
         self.player_list = arcade.SpriteList()
 
         # Set up the player
-        self.player_sprite = Player(":resources:images/animated_characters/female_person/femalePerson_idle.png", SPRITE_SCALING)
-        self.player_sprite.center_x = 50
-        self.player_sprite.center_y = 50
+        self.player_sprite = Player(":resources:images/alien/alienBlue_front.png", SPRITE_SCALING)
+        self.player_sprite.center_x = 30
+        self.player_sprite.center_y = 30
         self.player_list.append(self.player_sprite)
 
     def on_draw(self):
