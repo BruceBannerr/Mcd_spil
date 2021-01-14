3664ecbc40784a257c4e70a13025336c3868284f
christinahsn <chri16g7@study.sde.dk>
2021-01-14 08:15:14 +0100

    def key

--- BoardGame.py
+++ BoardGame.py
@@ -1,1 +1,13 @@
 import arcade
+
+def on_key_release(self, key, modifiers):
+        """Called when the user releases a key. """
+
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = 0
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = 0
