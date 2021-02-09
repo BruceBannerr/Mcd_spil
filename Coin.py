3d73a48836e1e710c82e8037aec3d153c2a46325
christinahsn <chri16g7@study.sde.dk>
2021-02-09 11:34:18 +0100

    Coin sidste commit

--- Coin.py
+++ Coin.py
@@ -79,7 +79,7 @@ class MyGame(arcade.Window):
 
             # Create the coin instance
             # Coin image from kenney.nl
-            coin = arcade.Sprite(":resources:images/items/coinGold.png", SPRITE_SCALING_COIN)
+            coin = arcade.Sprite(":resources:images/items/coinGold.png", COIN_SCALING)
 
             # --- IMPORTANT PART ---
 
